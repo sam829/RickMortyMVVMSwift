00a38cf6ea6bae8721c7158befff66eb7f2040bf
Saumya Macwan <macwansaumya2@gmail.com>
2023-05-01 23:49:09 +0530

    Initial Commit

---
+++ RickMortyDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickMortyDemo
+//
+//  Created by Saumya Macwan on 01/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
